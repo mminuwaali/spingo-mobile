a9ab5e8144943746348990aecafddc869ccae28f
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-07-20 16:56:08 +0100

    configured the global layout for the pages

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,13 @@
-import { } from "expo-router";
+import React from "react";
+import { Slot } from "expo-router";
 import { StyleSheet } from "react-native";
+import { hideAsync, preventAutoHideAsync } from "expo-splash-screen";
 
-export default function Layout() { };
+preventAutoHideAsync();
+export default function Layout() {
+    React.useEffect(() => { hideAsync(); }, []);
+
+    return <Slot />;
+};
 
 const styles = StyleSheet.create({});
