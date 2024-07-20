53427223ceea222f45d330c049d9258cf4ccc162
Muhammad Muhamamd Inuwa <mminuwaali@gmail.com>
2024-07-20 16:54:10 +0100

    setup type declarations

---
+++ src/app.d.ts
@@ -0,0 +1,13 @@
+declare type CategoryType = {
+    id: number
+    name: string
+}
+
+declare type ProductType = {
+    id: number
+    image: any
+    name: sring
+    price: number
+    category: CategoryType[]
+
+}
